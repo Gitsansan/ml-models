f049095828a9168f6832deefe465a723d3a408e0
Sandesh S <apple99450@gmail.com>
2025-09-25 16:34:01 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -8,6 +8,11 @@ from sklearn.tree import DecisionTreeClassifier
 
 my_data = pd.read_csv('datasets/drug200.csv')
 
+label_encoder = LabelEncoder()
+my_data['Sex'] = label_encoder.fit_transform(my_data['Sex']) 
+my_data['BP'] = label_encoder.fit_transform(my_data['BP'])
+my_data['Cholesterol'] = label_encoder.fit_transform(my_data['Cholesterol']) 
 
 print(my_data.head())
 print(my_data.isnull().sum())
+print("Completed")
