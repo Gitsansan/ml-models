d33063d6d870d14a9e2617fa7cab7874f7038a6f
Sandesh S <apple99450@gmail.com>
2025-09-27 00:33:25 +0530

    Updated the Decision Tree
    There's still a lot of changes to make, but for now, the program will train a new decision tree with the accuracy of 98%

--- decision_tree.py
+++ decision_tree.py
@@ -1,18 +1,40 @@
 # Required importrs
 
+print("------Decision Tree------")
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
 
+
+print("\nReading in the data...")
 my_data = pd.read_csv('datasets/drug200.csv')
 
+print("Preprocessing the data...")
 label_encoder = LabelEncoder()
 my_data['Sex'] = label_encoder.fit_transform(my_data['Sex']) 
 my_data['BP'] = label_encoder.fit_transform(my_data['BP'])
 my_data['Cholesterol'] = label_encoder.fit_transform(my_data['Cholesterol']) 
 
-print(my_data.head())
-print(my_data.isnull().sum())
-print("Completed")
+custom_map = {'drugA':0,'drugB':1,'drugC':2,'drugX':3,'drugY':4}
+my_data['Drug_num'] = my_data['Drug'].map(custom_map)
+
+
+print("\nSplitting the data into training and testing sets...")
+y = my_data['Drug']
+X = my_data.drop(['Drug','Drug_num'], axis=1)
+
+X_trainset, X_testset, y_trainset, y_testset = train_test_split(X, y, test_size=0.3, random_state=32)
+
+
+print("Training the Decision Tree model...")
+drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+
+drugTree.fit(X_trainset,y_trainset)
+print("\nDecision Tree has been trained.")
+
+tree_predictions = drugTree.predict(X_testset)
+print("Model Accuracy:", round(metrics.accuracy_score(y_testset, tree_predictions)*100,3), "%")
